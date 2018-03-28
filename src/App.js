0d5d5ffd30675ecf56a5e671b180917afdfd7fb3
maxobrien <max@maxobrien.co.uk>
2018-03-28 21:05:25 +0100

    Intial config setup with an example of import in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import {main, colours} from './config.js'
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>{"I'm the main container"}</h1>
+        <p>{main.name}</p>
+        <p>{colours.test}</p>
       </div>
     );
   }
