d2d0cdce14778335cc92cfdba9e6eb7b5cbcdac8
maxobrien <max@maxobrien.co.uk>
2018-03-28 23:18:52 +0100

    Some intial font-awesome testing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import './App.css';
 
 import {main, colours} from './config.js'
 
+import fontawesome from '@fortawesome/fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+
+import home from '@fortawesome/fontawesome-free-solid/faHome'
+import book from '@fortawesome/fontawesome-free-solid/faBook'
+import barcode from '@fortawesome/fontawesome-free-solid/faBarcode'
+import users from '@fortawesome/fontawesome-free-solid/faUsers'
+
+fontawesome.library.add(home, book, barcode, users)
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>{"I'm the main container"}</h1>
-        <p>{main.name}</p>
-        <p>{colours.test}</p>
+        <FontAwesomeIcon icon="home"/>
       </div>
     );
   }
--- src/config.js
+++ src/config.js
@@ -1,34 +1,59 @@
 /*
 
-    This is the main config file for Apollo.
+This is the main config file for Apollo.
 
-    The following objects are exported at the end
-        - main
-            - Contains some default values such as a long and short long name
-        - colours
-            - Contains all the colours that are used on the site.
-            - *PLEASE NOTE: This includes house colours for DCGS.
-            - Since the config cannot be imported into a CSS file, any colours should be set inline
-
+The following objects are exported at the end
+    - main
+        - Contains some default values such as a long and short long name
+    - colours
+        - Contains all the colours that are used on the site.
+        - *PLEASE NOTE: This includes house colours for DCGS.
+        - Since the config cannot be imported into a CSS file, any colours should be set inline
 */
 
 let main = {
-    shortName: "Apollo",
-    longName: "Apollo | Library Management System"
+	shortName: "Apollo",
+	longName: "Apollo | Library Management System",
+	pages: [
+		{
+			name: 'Home',
+			url: '/',
+			icon: 'home'
+		},
+		{
+			name: 'Catalogue',
+			url: '/catalogue',
+			icon: 'book'
+        },
+		{
+			name: 'Scan',
+			url: '/scan',
+			icon: 'barcode'
+        },
+		{
+			name: 'Students',
+			url: '/students',
+			icon: 'users'
+        }
+    ]
 };
 
 let colours = {
-    pink: "e44d95",
-    darkPink: "f160a6",
-    lightGrey: "f4f4f4",
-    darkGrey: "504f4f",
-    foxell: "fac800",
-    holman: "e97b1b",
-    newman: "d32b19",
-    pearson: "99c1e8",
-    rayner: "2a4597",
-    thorne: "267832"
+	pink: "e44d95",
+	darkPink: "f160a6",
+	lightGrey: "f4f4f4",
+	darkGrey: "504f4f",
+
+	foxell: "fac800",
+	holman: "e97b1b",
+	newman: "d32b19",
+	pearson: "99c1e8",
+	rayner: "2a4597",
+	thorne: "267832"
 };
 
 // Do the final export of every config object so it can be imported in other files
-export {main, colours};
+export {
+	main,
+	colours
+};
