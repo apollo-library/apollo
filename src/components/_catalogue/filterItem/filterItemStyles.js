3dd5f1d9e0f68c14374c369b9b97253fd5caf356
maxobrien <max@maxobrien.co.uk>
2018-05-26 17:37:38 +0100

    Updated colours of filter hover and selecting

--- src/components/_catalogue/filterItem/filterItemStyles.js
+++ src/components/_catalogue/filterItem/filterItemStyles.js
@@ -4,7 +4,7 @@ const FilterItem = styled.div`
     padding: 0.4rem 0.2rem 0.4rem 1rem;
     margin: 0.2rem 0;
     font-weight: 600;
-    transition: all 75ms linear;
+    transition: all 100ms linear;
     color: ${props => props.active ? props.theme.colours.primary : props.theme.colours.darkGrey};
     border-radius: ${props => props.theme.styles.borderRadius};
     display: flex;
@@ -13,7 +13,6 @@ const FilterItem = styled.div`
     cursor: pointer;
 
     &:hover {
-        color: ${props => props.theme.colours.primary};
         background: ${props => props.theme.colours.lightGrey};
     }
 `;
