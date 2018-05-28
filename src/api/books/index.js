e599689cd220f7b568637e67a1d71a7343dfdaf1
cameron-robey <cameron.robey@outlook.com>
2018-05-28 22:03:02 +0100

    add getScanBookInfo function and tweak getAllTags for API change

---
+++ src/api/books/index.js
@@ -0,0 +1,17 @@
+//Config
+import config from './../config.js';
+
+//Not have to refer to config everytime
+const serverPath = config.serverPath;
+
+async function getScanBookInfo(id) {
+    let data = await fetch(serverPath + '/book/' + id);
+    let json = await data.json();
+    if (json.code === "002") { return []; }
+    let response = await {author: json.data.author, title: json.data.title}
+    if (json.data.loanID) { response['loanID'] = json.data.loanID; }
+    return response; // <- return an object with all the tags
+}
+
+
+export { getScanBookInfo }
