76e31a0dafd2bd2552993b9b5f146dc8e6bef6fe
maxobrien <max@maxobrien.co.uk>
2018-05-28 18:14:13 +0100

    Fixed API reducer

--- src/store/reducers/apiReducer.js
+++ src/store/reducers/apiReducer.js
@@ -6,9 +6,6 @@ const initialStates = {
 
 export const api = (state = initialStates, action) => {
     switch (action.type) {
-        case TYPES.YOUR_TYPE_HERE:
-            return {...state, YOUR_THING_HERE }
-
         default:
             return state
     }
