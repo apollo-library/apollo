2ab6da7b3c354ca043f6bf8c79f149143d06d2bd
maxobrien <max@maxobrien.co.uk>
2018-05-28 21:37:16 +0100

    Fixed small warnings in console

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,10 @@
 import { createStore, combineReducers } from 'redux'
 import { data } from './reducers/dataReducer'
 import { ui } from './reducers/uiReducer'
-import { api } from './reducers/apiReducer'
 
 const rootReducer = combineReducers({
     data,
-    ui,
-    api
+    ui
 })
 
 export default createStore(rootReducer)
