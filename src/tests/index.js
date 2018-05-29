29d90effd15f6d959f39332f2669ba5689ec537c
cameron-robey <cameron.robey@outlook.com>
2018-05-29 15:10:18 +0100

    [API] Add renew book function

--- src/tests/index.js
+++ src/tests/index.js
@@ -5,7 +5,8 @@ import * as API from './../api/index.js';
 class Test extends Component {
     async componentDidMount() {
         //let fun = await API.Loans.returnBook('abc');
-        let fun = await API.Books.getScanBookInfo('abc');
+        //let fun = await API.Books.getScanBookInfo('abc');
+        let fun = await API.Loans.renewBook('abc', '2018-05-30');
         console.log(fun);
     }
 
