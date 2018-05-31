9d3ff9aacc0995a6ab57e7999096505d9ab61dbd
cameron-robey <cameron.robey@outlook.com>
2018-05-31 22:11:03 +0100

    [API] The withdrawBook function now functions like a functional function

--- src/tests/index.js
+++ src/tests/index.js
@@ -6,7 +6,8 @@ class Test extends Component {
     async componentDidMount() {
         //let fun = await API.Loans.returnBook('abc');
         //let fun = await API.Books.getScanBookInfo('abc');
-        let fun = await API.Loans.renewBook('abc', '2018-05-30');
+        //let fun = await API.Loans.renewBook('abc', '2018-05-30');
+        let fun = await API.Loans.withdrawBook('abc', 'zyx' '2018-05-30');
         console.log(fun);
     }
 
