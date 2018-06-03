815b39625a6332cd7f766287ec37bee9456c0b40
cameron-robey <cameron.robey@outlook.com>
2018-06-03 15:36:34 +0100

    [API] added some extra lines to make the console look a little neater

--- src/api/users/index.js
+++ src/api/users/index.js
@@ -1,10 +1,12 @@
 //Import functions
+// eslint-disable-next-line
 import * as Functions from './../_functions/';
 
 //Config
 import config from './../config.js';
 
 //Not have to refer to config everytime
+// eslint-disable-next-line
 const serverPath = config.serverPath;
 
 async function getAllUsers() {
