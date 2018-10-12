91e8055dd5d4499ae59a659a83ae98249bf6a692
Cameron Robey <cameron@cameronrobey.co.uk>
2018-10-13 00:33:05 +0100

    Link to book page from reports

--- src/components/_report/reportTable/reportTable.js
+++ src/components/_report/reportTable/reportTable.js
@@ -1,8 +1,10 @@
 //React imports
 import React, { Component } from 'react';
 
+
 //Styles
 import * as styles from './../../bookTable/bookTableStyles.js'
+import * as reportStyles from './reportTableStyles.js'
 
 import * as API from './../../../api';
 
@@ -35,7 +37,7 @@ class ReportTable extends Component {
             TableContent = this.props.data.data.map((rowData, index) =>
             (
                 <styles.TableRow key={index} colour={this.state.accent}>
-                    <styles.TableText>{rowData.display.title}</styles.TableText>
+                    <styles.TableText><reportStyles.TableLink to={'/book/' + rowData.raw.book._id}>{rowData.display.title}</reportStyles.TableLink></styles.TableText>
                     <styles.TableText>{rowData.display.author}</styles.TableText>
                     <styles.TableText><b>{rowData.display.reg}</b> {': ' + rowData.display.name}</styles.TableText>
                     <styles.TableText>{this.formatDueDate(rowData.display.due)}</styles.TableText>
