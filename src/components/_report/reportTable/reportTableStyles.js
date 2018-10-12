52f92e963677ce202f88d645662125b96e93112f
Cameron Robey <cameron@cameronrobey.co.uk>
2018-10-13 00:33:05 +0100

    Link to book page from reports

---
+++ src/components/_report/reportTable/reportTableStyles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+import {Link} from 'react-router-dom';
+
+const TableLink = styled(Link)`
+    text-decoration: none;
+    color: inherit;
+    font-weight: 600;
+`;
+
+export {
+    TableLink
+};
