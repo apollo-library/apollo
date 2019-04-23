c3312a21d2d6fa37a6ee7931653a4ee0e607b521
Cameron Robey <cameron@cameronrobey.co.uk>
2019-04-23 16:53:43 +0100

    Change colour of fines on the dashboard

--- src/components/_report/reports/loan.js
+++ src/components/_report/reports/loan.js
@@ -17,6 +17,7 @@ class Loan extends Component {
 
     componentDidMount = async () => {
         let data = await API.Loans.getLoans();
+        console.log(data)
         let tableData = data.data.map((item) => {
             return [
                 {style: "bold", url: "/book/" + item.raw.book._id, display: item.display.title},
