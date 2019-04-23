d1cd4fa77896f0f61f14143b6a5e7518eabd3630
maxobrien <max@maxobrien.co.uk>
2019-04-23 21:42:12 +0100

    300!

--- src/components/_dashboard/tags/tags.js
+++ src/components/_dashboard/tags/tags.js
@@ -64,13 +64,14 @@ class Tags extends Component {
 
     updateTags = async () => {
         let tags = await API.Tags.getAllTags();
-
+f 
         tags.sort( function( a, b ) {
             a = a.name.toLowerCase();
             b = b.name.toLowerCase();
 
             return a < b ? -1 : a > b ? 1 : 0;
         })
+
         this.setState({tags: tags})
     }
 
